e8bff4cebc96dfed5a47ab603fc44b5d1b2fe744
sonny <congbyrr@gmail.com>
2020-11-23 19:59:38 +0900

    test

--- app/src/main/java/com/homework/test1/MainActivity.java
+++ app/src/main/java/com/homework/test1/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
 
     @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
